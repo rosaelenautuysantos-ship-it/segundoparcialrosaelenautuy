207818d818dcf83f9e0ac1637e06b54357ed72f1
rosaelenautuysantos-ship-it <rosaelenautuysantos@gmail.com>
2026-04-15 13:01:10 -0600

    Add files via upload

---
+++ numero  mayor.py
@@ -0,0 +1,18 @@
+#Nombre del alumno: ROSA ELENA UTUY SANTOS
+#Nombre del trabajo: Numero mayor 
+#fecha :25 de marzo del 2026
+
+#pedir al usuario tres numeros y mostrar el mayor
+
+#Estructura  if 
+
+num1= float(input("ingresa el primer numero : "))
+num2 = float(input("ingresa el segundo numero : "))
+num3 = float(input("ingresa el tercer numero:  "))
+
+if num1>num2:
+    print("elnumero mayor es : " , num1)
+if num1>num3:
+    print("el numero mayor es " , num1)
+
+
